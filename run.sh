b5fff95977306688e52832f30bd27f482b863b7b
stoffelll <github@tm-solutions.de>
2026-06-14 18:20:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-export TELEGRAM_BOT_TOKEN=$(python -c "import json; print(json.load(open('/data/options.json'))['TELEGRAM_BOT_TOKEN'])")
-export PAPERLESS_URL=$(python -c "import json; print(json.load(open('/data/options.json'))['PAPERLESS_URL'])")
-export PAPERLESS_TOKEN=$(python -c "import json; print(json.load(open('/data/options.json'))['PAPERLESS_TOKEN'])")
+# Optionen als Root auslesen
+TELEGRAM_BOT_TOKEN=$(python -c "import json; print(json.load(open('/data/options.json'))['TELEGRAM_BOT_TOKEN'])")
+PAPERLESS_URL=$(python -c "import json; print(json.load(open('/data/options.json'))['PAPERLESS_URL'])")
+PAPERLESS_TOKEN=$(python -c "import json; print(json.load(open('/data/options.json'))['PAPERLESS_TOKEN'])")
 
-python -m paperless_bot
+export TELEGRAM_BOT_TOKEN
+export PAPERLESS_URL
+export PAPERLESS_TOKEN
+
+# Als originaler User (appuser) die App starten
+su -s /bin/bash -c 'export TELEGRAM_BOT_TOKEN="'"$TELEGRAM_BOT_TOKEN"'" PAPERLESS_URL="'"$PAPERLESS_URL"'" PAPERLESS_TOKEN="'"$PAPERLESS_TOKEN"'"; python -m paperless_bot' appuser
