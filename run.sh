f72eb98c0a7b68e10d8924190114de6dfff62a64
stoffelll <github@tm-solutions.de>
2026-06-14 18:30:31 +0200

    Update run.sh
    Telegram_allowed_users

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Optionen als Root auslesen
 export TELEGRAM_BOT_TOKEN=$(cat /data/options.json | python -c "import sys,json; print(json.load(sys.stdin)['TELEGRAM_BOT_TOKEN'])")
 export PAPERLESS_URL=$(cat /data/options.json | python -c "import sys,json; print(json.load(sys.stdin)['PAPERLESS_URL'])")
 export PAPERLESS_TOKEN=$(cat /data/options.json | python -c "import sys,json; print(json.load(sys.stdin)['PAPERLESS_TOKEN'])")
+export TELEGRAM_ALLOWED_USERS=$(cat /data/options.json | python -c "import sys,json; print(json.load(sys.stdin).get('TELEGRAM_ALLOWED_USERS',''))")
 
-# Als paperlessbot User starten mit den Umgebungsvariablen
 exec su -s /bin/sh -c "
   export TELEGRAM_BOT_TOKEN='$TELEGRAM_BOT_TOKEN'
   export PAPERLESS_URL='$PAPERLESS_URL'
   export PAPERLESS_TOKEN='$PAPERLESS_TOKEN'
+  export TELEGRAM_ALLOWED_USERS='$TELEGRAM_ALLOWED_USERS'
   python -m paperless_bot
 " paperlessbot
